b3cf5e1d3cff8087365520293eb820130c4bbebf
thomaz.capra <thomaz.capra@gmail.com>
2019-08-27 22:14:51 +0100

    Updates

---
+++ precache-manifest.9a9f547d89aa03764e809cfd75004ecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbe35dab60c4665f6af82df948c8aa4",
+    "url": "/thomazcapra/mglass/index.html"
+  },
+  {
+    "revision": "39f02a5ebb0354c2543f",
+    "url": "/thomazcapra/mglass/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eccc2fcacbfe09ffe37d",
+    "url": "/thomazcapra/mglass/static/js/2.82348c90.chunk.js"
+  },
+  {
+    "revision": "39f02a5ebb0354c2543f",
+    "url": "/thomazcapra/mglass/static/js/main.42c3637e.chunk.js"
+  },
+  {
+    "revision": "84c671252e949ccd11df",
+    "url": "/thomazcapra/mglass/static/js/runtime~main.00415a67.js"
+  }
+]);

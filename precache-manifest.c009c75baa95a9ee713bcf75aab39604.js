1dfc4faea6f7417a06df6fc393016be8cbb3dba0
Thomaz Capra <x190823@millenniumbcp.pt>
2019-08-28 09:16:17 +0100

    Updates

---
+++ precache-manifest.c009c75baa95a9ee713bcf75aab39604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf51b7d72ad5f0581ca657638ddd2aa",
+    "url": "/thomazcapra/mglass/index.html"
+  },
+  {
+    "revision": "7d677dce314d09b0f198",
+    "url": "/thomazcapra/mglass/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c72cc1f073d8ab262f97",
+    "url": "/thomazcapra/mglass/static/js/2.1bc1b693.chunk.js"
+  },
+  {
+    "revision": "7d677dce314d09b0f198",
+    "url": "/thomazcapra/mglass/static/js/main.d1178823.chunk.js"
+  },
+  {
+    "revision": "84c671252e949ccd11df",
+    "url": "/thomazcapra/mglass/static/js/runtime~main.00415a67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.439e0fce99d52887051caf516e0b3870.js"
+  "/thomazcapra/mglass/precache-manifest.c009c75baa95a9ee713bcf75aab39604.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thomazcapra/mglass/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
